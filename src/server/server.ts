1f2eaa8675da0a4039a28fc897d40340c320bb6f
Uri <uri555459@gmail.com>
2024-09-03 10:17:16 +0300

    Adde helmet

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import 'dotenv/config'
 import express from 'express'
+import helmet from 'helmet'
 import morgan from 'morgan'
 
 import { parserRouter, sendRouter } from './routes'
@@ -9,9 +10,10 @@ import { authRouter } from './routes/auth.route'
 export const startServer = async () => {
 	const app = express()
 
-	const port = process.env.PORT
-	const hostNameServer = process.env.HOST_NAME_SERVER
+	const port = process.env.PORT || 4200
+	const hostNameServer = process.env.HOST_NAME_SERVER || 'http://localhost'
 
+	app.use(helmet())
 	app.use(cors())
 	app.use(morgan('dev'))
 	app.use(express.urlencoded({ extended: true }))
